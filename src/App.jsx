6eba7bfd65e45cc78332b7d4b9c468dd46ae78d1
olawalecoder <bolawale81@gmail.com>
2025-01-14 21:12:57 +0100

    Add web project assets

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
     return (
-        <div className="bg-amber-100">App</div>
+        <div className="text-6xl text-violet-300 bg-amber-100 font-zentry min-h-dvh">App</div>
     )
 }
 export default App
